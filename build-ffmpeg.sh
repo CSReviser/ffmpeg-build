2f8d1fe6941022d730242600710d5e592165215a
CSReviser <46049273+CSReviser@users.noreply.github.com>
2025-05-08 15:43:12 +0900

    build-ffmpeg.sh を作成

---
+++ build-ffmpeg.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+FFMPEG_VERSION=6.0
+NDK=/opt/android-ndk
+SYSROOT=$NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot
+TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
+
+ARCH=arm
+CPU=armv7-a
+API=21
+TARGET=armv7a-linux-androideabi
+PREFIX=$(pwd)/android/$ARCH
+
+mkdir -p $PREFIX
+
+curl -L https://ffmpeg.org/releases/ffmpeg-$FFMPEG_VERSION.tar.gz | tar xz
+cd ffmpeg-$FFMPEG_VERSION
+
+./configure \
+  --prefix=$PREFIX \
+  --target-os=android \
+  --arch=$ARCH \
+  --cpu=$CPU \
+  --cc=$TOOLCHAIN/bin/${TARGET}${API}-clang \
+  --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+  --enable-cross-compile \
+  --sysroot=$SYSROOT \
+  --enable-gpl \
+  --enable-static \
+  --disable-shared \
+  --disable-doc \
+  --disable-programs \
+  --disable-avdevice \
+  --disable-postproc
+
+make -j$(nproc)
+make install
